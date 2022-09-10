cb508f71b4028bd1a383c0c0743754cf93dd7157
TextAdventurer12 <drthirtle@gmail.com>
2022-09-11 10:05:12 +1200

    removing temp files

--- macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef MACRO_H
-#define MACRO_H
-
-#define min(a, b) {typeof(a) min_a = a; typeof(b) min_b = b; typeof(a) == typeof(b) ? min_a < min_b ? min_a : min_b : 0};
-#define max(a, b) {typeof(a) max_a = a; typeof(b) max_b = b; typeof(a) == typeof(b) ? max_a > max_b ? max_a : max_b : 0};
-
-#define loop(var, max) for(int var=0;var<max;var++)
-
-#endif
